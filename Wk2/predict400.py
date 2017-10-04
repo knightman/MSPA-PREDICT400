0be78579752e581664dc132a1c101a57a702aa35
Andrew Knight <andrewknight@Telular.Telular.com>
2017-10-03 23:19:04 -0500

    additions and cleanup

---
+++ Wk2/predict400.py
@@ -0,0 +1,14 @@
+# predict400.py
+# Class I created to simplify repetitive tasks for MSPA PREDICT400
+
+# if __name__ == "__main__":
+#     import sys
+#     arg1 = sys.argv[1]
+import numpy as np
+
+class Predict400:
+    def __init__(self):
+        self.msg1 = 'ok'
+    
+    def solve():
+        print(self.msg1)
